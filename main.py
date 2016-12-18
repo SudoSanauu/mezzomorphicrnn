37c836290ce616f108d47247bb9ac0c1274cdfb1
Aaron Jacobson <aaron.the.cow@gmail.com>
2016-12-17 17:46:26 -0800

    create and plot predictions from RNN

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import numpy as np
 import wave
-# import scipy
-# import matplotlib.pyplot as matplot
+import scipy
+import matplotlib.pyplot as plot
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
+import math
 
 
 def byte_to_float(input_byte):
@@ -89,10 +90,10 @@ testX, testY = create_dataset(test, look_back)
 trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
 testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
 
-print("TrainX: ")
-print(trainX)
-print("TestX: ")
-print(testX)
+# print("TrainX: ")
+# print(trainX)
+# print("TestX: ")
+# print(testX)
 
 
 
@@ -105,4 +106,52 @@ model.add(Dense(1))
 # CHECK WHAT OPTIMIZER MEANS
 model.compile(loss='mean_squared_error', optimizer='adam',metrics=['accuracy'])
 
-model.fit(trainX, trainY, nb_epoch=100, batch_size=1, verbose=1)
+model.fit(trainX, trainY, nb_epoch=1, batch_size=1, verbose=1)
+
+train_predict = model.predict(trainX)
+test_predict = model.predict(testX)
+
+# print("train_predict")
+# print(train_predict)
+# print("test_predict")
+# print(test_predict)
+
+train_predict = scaler.inverse_transform(train_predict)
+trainY = scaler.inverse_transform(trainY)
+test_predict = scaler.inverse_transform(test_predict)
+testY = scaler.inverse_transform(testY)
+
+# print("train_predict")
+# print(train_predict)
+# print("test_predict")
+# print(test_predict)
+
+# print("trainY[0]: ")
+# print(trainY[0])
+
+# train_score = math.sqrt(mean_squared_error(trainY[0], train_predict[:,0]))
+# print('Train Score: %.2f RMSE' % (train_score))
+# test_score = math.sqrt(mean_squared_error(testY[0], test_predict[:,0]))
+# print('Train Score: %.2f RMSE' % (test_score))
+
+
+
+
+
+
+
+
+
+# shift train predictions for plotting
+train_predict_plot = np.empty_like(audio_dataset)
+train_predict_plot[:, :] = np.nan
+train_predict_plot[look_back:len(train_predict)+look_back, :] = train_predict
+# shift test predictions for plotting
+test_predict_plot = np.empty_like(audio_dataset)
+test_predict_plot[:, :] = np.nan
+test_predict_plot[len(train_predict)+(look_back*2)+1:len(audio_dataset)-1, :] = test_predict
+# plot baseline and predictions
+plt.plot(scaler.inverse_transform(audio_dataset))
+plt.plot(train_predict_plot)
+plt.plot(test_predict_plot)
+plt.show()
